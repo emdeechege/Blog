4d2759f6ed1523c301269c36b83c2c2dab558b45
Tom Chege <emdeechege@gmail.com>
2018-09-13 11:55:10 +0300

    added login parameters in views py

---
+++ app/main/views.py
@@ -0,0 +1,57 @@
+from flask import render_template
+from flask import render_template,request,redirect,url_for
+from flask_login import login_required,current_user
+from ..models import *
+from . import main
+from .. import db,photos
+from .forms import *
+
+@main.route('/')
+def index():
+    '''
+    my index page
+    return
+    '''
+    message= "Hello"
+    title= "Emdee's Blog"
+    return render_template('index.html', message=message,title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(author = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(author = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.author))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(author = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
