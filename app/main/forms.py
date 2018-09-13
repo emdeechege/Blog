118052b2160e09bdb05752e6799185c7171140b8
Tom Chege <emdeechege@gmail.com>
2018-09-13 12:00:32 +0300

    authorization files added

---
+++ app/main/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about yourself.',validators = [Required()])
+    submit = SubmitField('Submit')
